53230538c7fbaec17cbf70455db7aa273e709b89
scolman <scolman@myune.edu.au>
2023-08-20 16:29:34 +1000

    style fix

--- src/components/Digital.jsx
+++ src/components/Digital.jsx
@@ -5,11 +5,11 @@ function Digital(props) {
   return (
     <div className="digital-container">
       <div className="digit-wrapper">
-        <div className="digit-box">{hours > 9 ? hours : `0${hours}`}</div>
-        <span>:</span>
-        <div className="digit-box">{minutes > 9 ? minutes : `0${minutes}`}</div>
-        <span>:</span>
-        <div className="digit-box">{seconds > 9 ? seconds : `0${seconds}`}</div>
+        <div>{hours > 9 ? hours : `0${hours}`}:</div>
+        <div>{minutes > 9 ? minutes : `0${minutes}`}:</div>
+        <div className="seconds-wrapper">
+          {seconds > 9 ? seconds : `0${seconds}`}
+        </div>
       </div>
     </div>
   )
